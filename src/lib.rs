d035c22d5aae47f4f967adfa5fb8654bbddae30b
lordpaijo <iyanmaumakan56@gmail.com>
2025-03-09 20:54:09 +0700

    nyoba-nyoba readme

--- src/lib.rs
+++ src/lib.rs
@@ -19,13 +19,13 @@ mod tests {
         //  Kalau memang dibutuhkan, juga bisa buat ngasih contoh sintaks.
         // * ============================================================ *
         
-        use crate::sistem::kelipatan::*; 
-        use crate::sistem::bilangan::*;
+        use crate::sistem::kelipatan; 
+        use crate::sistem::bilangan;
 
-        let x = 6.5; let y: i64 = 8;
-        let z: i64 = bulat(x) as i64;
-        genap("cek", kpk(z, y));
-        ganjil("rubah", fpb(z, y));
+        let x = 6.7487; let y: i64 = 8;
+        let z: i64 = bilangan::bulat(x) as i64;
+        bilangan::genap("cek", kelipatan::kpk(z, y));
+        bilangan::ganjil("rubah", kelipatan::fpb(z, y));
 
     }
 }
