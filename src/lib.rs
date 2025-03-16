5a327ac1cb2518541c632aa4f7ca55f6c5b2f27a
lordpaijo <iyanmaumakan56@gmail.com>
2025-03-16 21:15:07 +0700

    segitiga, lingkaran, dan bola. \src/sistem/geometri.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ mod tests {
         use crate::sistem::bilangan;
         use crate::sistem::fibonacci::*;
         use crate::sistem::aljabar::*;
+        use crate::sistem::geometri::bangun_datar::*;
 
         let x = 6.7487; let y: i64 = 8;
         let z: i64 = bilangan::bulat(x) as i64;
@@ -20,5 +21,8 @@ mod tests {
         bilangan::ganjil("rubah", kelipatan::fpb(z, y));
         Fibonacci::iteratif(10);
         assert_eq!(SistemPersamaan::splsv(6.0, 12.0), Some(-2.0));
+        let sisitiga = [3.00, 3.00, 3.00];
+        let segitiga = Segitiga::new(sisitiga[0], 4.00, sisitiga);
+        assert_eq!(segitiga.keliling(), 9.00);
     }
 }
--- src/sistem/geometri.rs
+++ src/sistem/geometri.rs
@@ -1,4 +1,4 @@
-pub mod dua_dimensi 
+pub mod bangun_datar 
 { 
     pub struct Persegi
     {
@@ -45,9 +45,55 @@ pub mod dua_dimensi
             (self.panjang + self.lebar) * 2.00 
         }
     }
+
+    pub struct Segitiga
+    {
+        alas: f64, tinggi: f64, sisi: [f64; 3]
+    }
+
+    impl Segitiga
+    {
+        pub fn new (alas: f64, tinggi: f64, sisi: [f64; 3]) -> Self
+        {
+            Self { alas, tinggi, sisi }
+        }
+
+        pub fn luas (&self) -> f64
+        {
+            (1.00/2.00) * self.alas * self.tinggi
+        }
+
+        pub fn keliling (&self) -> f64
+        {
+            self.sisi[0] + self.sisi[1] + self.sisi[2]
+        }
+    }
+
+    pub struct Lingkaran
+    {
+        r: f64,
+    }
+
+    impl Lingkaran
+    {
+        pub fn new (r: f64) -> Self
+        {
+            Self { r }
+        }
+
+        pub fn luas (&self) -> f64
+        {
+            (22.00 / 7.00) * self.r.powf(2.00)
+        }
+
+        pub fn keliling (&self) -> f64
+        {
+            2.00 * (22.00 / 7.00) * self.r
+        }
+    }
 }
 
-pub mod tiga_dimensi 
+pub mod bangun_ruang 
 {
     pub struct Kubus 
     {
@@ -137,6 +183,39 @@ pub mod tiga_dimensi
         pub fn diagonal_ruang (&self) -> f64
         {
             (self.panjang.powf(2.00) + self.lebar.powf(2.00) + self.tinggi.powf(2.00)).sqrt()
+        }
+    }
+
+    pub struct Bola
+    {
+        r: f64,
+    }
+
+    impl Bola
+    {
+        pub fn new (r: f64) -> Self
+        {
+            Self { r }
+        }
+
+        pub fn luas_permukaan (&self) -> f64
+        {
+            4.00 * (22.00 / 7.00) * self.r.powf(2.00)
+        }
+
+        pub fn volume (&self) -> f64
+        {
+            (4.00 / 3.00) * (22.00 / 7.00) * self.r.powf(3.00)
+        }
+
+        pub fn setengah_volume (&self) -> f64
+        {
+            (2.00 / 3.00) * (22.00 / 7.00) * self.r
+        }
+
+        pub fn keliling (&self) -> f64
+        {
+            (4.00 / 3.00) * (22.00 / 7.00) * self.r.powf(2.00)
         }
     }
 }
