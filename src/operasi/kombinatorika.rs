3f4c64991e1c051a16c6b1f32e3f64ad5c4c00b1
lordpaijo <iyanmaumakan56@gmail.com>
2025-03-19 11:59:03 +0700

    Peluang > Koin  `\src/operasi/kombinatorika.rs`

--- src/operasi/kombinatorika.rs
+++ src/operasi/kombinatorika.rs
@@ -53,4 +53,38 @@ pub mod Peluang
             count as f64 / self.angka.len() as f64
         }
     }
+
+    pub struct Koin 
+    {
+        pub sisi: [char; 2],
+    }
+
+    impl Koin 
+    {
+        pub fn new() -> Self 
+        {
+            Self { sisi: ['H', 'T'], /* H = Kepala, T = Ekor */ }
+        }
+
+        pub fn peluang(&self, target: char) -> f64 
+        {
+            if self.sisi.contains(&target) {
+                1.0 / self.sisi.len() as f64
+            } else { 0.0 }
+        }
+
+        pub fn peluang_beruntun(&self, target: char, jumlah: u32) -> f64 
+        {
+            if self.sisi.contains(&target) {
+                (1.0 / self.sisi.len() as f64).powi(jumlah as i32)
+            } else { 0.0 }
+        }
+
+        pub fn peluang_setidaknya_satu(&self, target: char, jumlah: u32) -> f64 
+        {
+            if self.sisi.contains(&target) {
+                1.0 - self.peluang_beruntun(if target == 'H' { 'T' } else { 'H' }, jumlah)
+            } else { 0.0 }
+        }
+    }
 }
