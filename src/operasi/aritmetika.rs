c549a675f9697bd12a782d852c9fbced52169bd6
lordpaijo <iyanmaumakan56@gmail.com>
2025-03-20 17:18:51 +0700

    pergantian dari `#[allow(dead_code)]` menjadi `#![allow(dead_code)]` -> `\src/operasi/aritmetika.rs`

--- src/operasi/aritmetika.rs
+++ src/operasi/aritmetika.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::f64;
 use std::ops::*;
 use std::collections::HashMap;
@@ -19,7 +21,6 @@ pub fn pangkat<T>(base: T, exp: u32) -> T  where T: Mul<Output = T> + Copy + One
     hasil
 }
 
-#[allow(dead_code)]
 pub fn akar_kuadrat(x: f64) -> f64 
 {
     if x < 0.0 { panic!("Akar kuadrat dari bilangan negatif tidak terdefinisi untuk f64."); }
@@ -28,18 +29,12 @@ pub fn akar_kuadrat(x: f64) -> f64
     z
 }
 
-#[allow(dead_code)]
 pub fn tambah <T: Add<Output = T>> (a: T, b: T) -> T { a + b }
-#[allow(dead_code)]
 pub fn kurang <T: Sub<Output = T>> (a: T, b: T) -> T { a - b }
-#[allow(dead_code)]
 pub fn kali   <T: Mul<Output = T>> (a: T, b: T) -> T { a * b }
-#[allow(dead_code)]
 pub fn bagi   <T: Div<Output = T>> (a: T, b: T) -> T { a / b }
-#[allow(dead_code)]
 pub fn modulo <T: Rem<Output = T>> (a: T, b: T) -> T { a % b }
 
-#[allow(dead_code)]
 pub fn pangkat_optim<T> (mut base: T, mut exp: u32) -> T  
 where T: Mul<Output = T> + Copy + One,
 {
@@ -56,13 +51,11 @@ where T: Mul<Output = T> + Copy + One,
     hasil
 }
 
-#[allow(dead_code)]
 pub fn pangkat_desimal (base: f64, exp: f64) -> f64 
 {
     base.powf(exp)
 }
 
-#[allow(dead_code)]
 pub fn akar_pangkat_n (x: f64, n: f64) -> f64 
 {
     if x < 0.0 && n % 2.0 == 0.0 
@@ -72,7 +65,6 @@ pub fn akar_pangkat_n (x: f64, n: f64) -> f64
     x.powf(1.0 / n)
 }
 
-#[allow(dead_code)]
 pub fn logaritma (x: f64, base: f64) -> f64 
 {
     if x <= 0.0 || base <= 0.0 || base == 1.0 
@@ -83,7 +75,7 @@ pub fn logaritma (x: f64, base: f64) -> f64
 }
 
 /* Super (Lebih dari dua angka, menggunakan array atau vector) */
-#[allow(dead_code)]
+
 pub fn super_tambah <T: Add<Output = T> + Copy> (angka: &[T]) -> T 
 { 
     let mut total = angka[0];
@@ -91,7 +83,6 @@ pub fn super_tambah <T: Add<Output = T> + Copy> (angka: &[T]) -> T
     total 
 }
 
-#[allow(dead_code)]
 pub fn super_kurang <T: Sub<Output = T> + Copy> (angka: &[T]) -> T 
 { 
     let mut total = angka[0];
@@ -100,7 +91,6 @@ pub fn super_kurang <T: Sub<Output = T> + Copy> (angka: &[T]) -> T
 
 }
 
-#[allow(dead_code)]
 pub fn super_kali <T: Mul<Output = T> + Copy> (angka: &[T]) -> T 
 { 
     let mut total = angka[0];
@@ -109,7 +99,6 @@ pub fn super_kali <T: Mul<Output = T> + Copy> (angka: &[T]) -> T
   
 }
 
-#[allow(dead_code)]
 pub fn super_bagi <T: Div<Output = T> + Copy> (angka: &[T]) -> T 
 { 
     let mut total = angka[0];
@@ -118,7 +107,6 @@ pub fn super_bagi <T: Div<Output = T> + Copy> (angka: &[T]) -> T
   
 }
 
-#[allow(dead_code)]
 pub fn super_logaritma (angka: &[f64], basis: f64) -> Vec<f64> 
 {
     if basis <= 0.0 || basis == 1.0 
@@ -136,7 +124,6 @@ pub fn super_logaritma (angka: &[f64], basis: f64) -> Vec<f64>
     }).collect()
 }
 
-#[allow(dead_code)]
 pub fn mean (angka: &[f64]) -> f64 
 {
     if angka.is_empty() 
@@ -147,8 +134,6 @@ pub fn mean (angka: &[f64]) -> f64
     total / angka.len() as f64
 }
 
-
-#[allow(dead_code)]
 pub fn median (angka: &mut [f64]) -> f64 
 {
     if angka.is_empty() 
@@ -163,7 +148,6 @@ pub fn median (angka: &mut [f64]) -> f64
     else { (angka[n / 2 - 1] + angka[n / 2]) / 2.0 }
 }
 
-#[allow(dead_code)]
 pub fn modus (angka: &[i32]) -> Vec<i32> 
 {
     if angka.is_empty() 
