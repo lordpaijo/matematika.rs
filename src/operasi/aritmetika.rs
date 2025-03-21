f8ddf7ff6e4a1078e4526ab3ec124e564f88dca8
lordpaijo <iyanmaumakan56@gmail.com>
2025-03-21 10:54:53 +0700

    pemisahan modul statistika dari `\src/operasi/aritmetika.rs` -> `\src/operasi/statistika.rs`

--- src/operasi/aritmetika.rs
+++ src/operasi/aritmetika.rs
@@ -1,6 +1,5 @@
 use std::f64;
 use std::ops::*;
-use std::collections::HashMap;
 
 /* Normal (hanya dua angka) */
 
@@ -122,47 +121,3 @@ pub fn super_logaritma (angka: &[f64], basis: f64) -> Vec<f64>
     }).collect()
 }
 
-pub fn mean (angka: &[f64]) -> f64 
-{
-    if angka.is_empty() 
-    {
-        panic!("Tidak bisa menghitung rata-rata dari array kosong.");
-    }
-    let total: f64 = angka.iter().sum();
-    total / angka.len() as f64
-}
-
-pub fn median (angka: &mut [f64]) -> f64 
-{
-    if angka.is_empty() 
-    {
-        panic!("Tidak dapat menghitung median dari data kosong.");
-    }
-    
-    angka.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let n = angka.len();
-    
-    if n % 2 == 1{ angka[n / 2] } 
-    else { (angka[n / 2 - 1] + angka[n / 2]) / 2.0 }
-}
-
-pub fn modus (angka: &[i32]) -> Vec<i32> 
-{
-    if angka.is_empty() 
-    {
-        panic!("Tidak dapat menghitung modus dari data kosong.");
-    }
-
-    let mut frekuensi = HashMap::new();
-    for &num in angka 
-    {
-        *frekuensi.entry(num).or_insert(0) += 1;
-    }
-
-    let max_freq = frekuensi.values().copied().max().unwrap();
-    frekuensi
-        .into_iter()
-        .filter(|&(_, count)| count == max_freq)
-        .map(|(num, _)| num)
-        .collect()
-}
--- src/operasi/mod.rs
+++ src/operasi/mod.rs
@@ -1,6 +1,6 @@
 pub mod aritmetika;
 pub mod kombinatorika;
-
+pub mod statistika;
 
 // #[allow(dead_code)]
 // pub use aritmetika::*;
