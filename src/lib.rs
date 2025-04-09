2f8eaaf4ce5bfb88fc2f62dd1ae9d15b0bab3aa5
lordpaijo <iyanmaumakan56@gmail.com>
2025-04-09 09:38:09 +0700

    Patch: Fibonacci >> aritmetika.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ mod tests {
 
 
         use crate::operasi::aritmetika;
-        use crate::sistem::fibonacci::*;
         use crate::sistem::aljabar::*;
         use crate::sistem::geometri::bangun_datar::*;
 
@@ -19,7 +18,7 @@ mod tests {
         let z: i64 = aritmetika::bulat(x) as i64;
         aritmetika::genap("cek", aritmetika::kpk(z, y));
         aritmetika::ganjil("rubah", aritmetika::fpb(z, y));
-        Fibonacci::iteratif(10);
+        aritmetika::Fibonacci::iteratif(10);
         assert_eq!(SistemPersamaan::splsv(6.0, 12.0), Some(-2.0));
         let sisitiga = [3.00, 3.00, 3.00];
         let segitiga = Segitiga::new(sisitiga[0], 4.00, sisitiga);
--- src/sistem/mod.rs
+++ src/sistem/mod.rs
@@ -1,9 +1,7 @@
 pub mod basis;
 pub mod geometri;
-pub mod fibonacci;
 pub mod aljabar;
 
 pub use basis::*;
 pub use geometri::*;
-pub use fibonacci::*;
 pub use aljabar::*;
