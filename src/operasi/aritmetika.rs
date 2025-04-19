938722a18d2849682712a5270528ff8a87f9c4bd
Iyan <iyanmaumakan56@gmail.com>
2025-04-19 22:02:24 +0700

    chore: Adding float to trait zero and uint to trait one.

--- src/operasi/aritmetika.rs
+++ src/operasi/aritmetika.rs
@@ -6,12 +6,16 @@ use std::ops::*;
 pub trait Zero { fn zero() -> Self; }
 
 impl Zero for u32 { fn zero() -> Self { 0 } }
-impl Zero for i32 { fn zero() -> Self { 0 } }
 impl Zero for u64 { fn zero() -> Self { 0 } }
+impl Zero for i32 { fn zero() -> Self { 0 } }
 impl Zero for i64 { fn zero() -> Self { 0 } }
+impl Zero for f32 { fn zero() -> Self { 1.0 } }
+impl Zero for f64 { fn zero() -> Self { 1.0 } }
 
 pub trait One { fn one() -> Self; }
 
+impl One for u32 { fn one() -> Self { 0 } }
+impl One for u64 { fn one() -> Self { 0 } }
 impl One for i32 { fn one() -> Self { 1 } }
 impl One for i64 { fn one() -> Self { 1 } }
 impl One for f32 { fn one() -> Self { 1.0 } }
